0983d4792d14b02aad72f5f51a8412958df2e1ee
Poulami-123 <kpoulami04@gmail.com>
2022-10-12 15:08:52 +0000

    first commit

---
+++ src/main/java/com/accenture/maven/Activity/Treemapactivity.java
@@ -0,0 +1,23 @@
+package com.accenture.maven.Activity;
+import java.util.NavigableMap;
+import java.util.*;
+
+public class Treemapactivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub9(
+		//TreeMap<Integer,String> t= new TreeMap<Integer,String>(Collections.reverseOrder());
+		NavigableMap<Integer,String> t=new TreeMap<Integer,String>();
+		t.put(1, "Rahul");
+		t.put(2,"Raj");
+		t.put(3, "Polo");
+		t.put(4, "Sneha");
+		System.out.println(t.descendingMap());
+		
+		
+				
+		
+
+	}
+
+}
